1d672474f8ac2868b29f0aa1572da71c6f658aa8
Akhdan Habibie <60088854+Akhdan1998@users.noreply.github.com>
2022-09-01 23:26:29 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:paymentmethod/bca.dart';
+import 'package:get/get.dart';
+import 'package:paymentmethod/permata.dart';
+import 'bni.dart';
+import 'bri.dart';
+import 'mandiri.dart';
+
+void main() {
+  runApp (MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: paymentmethodbca(),
+    );
+  }
+}
